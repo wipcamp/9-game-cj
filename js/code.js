d781aa3c03c9dda22406e399e8282ff5fd80e0d7
poom029 <poom_22love@hotmail.com>
2017-02-09 23:05:12 +0700

    renovate algorithm

--- js/code.js
+++ js/code.js
@@ -9,19 +9,31 @@ function preload() {
     game.load.image('enemy_ship','images/enemyship.png');
     game.load.image('background','images/sea.png');
     game.load.image('laser','images/biglaser.png');
+    game.load.spritesheet('up','images/up.png',180,180);
+    game.load.spritesheet('left','images/left.png',180,180);
+    game.load.spritesheet('right','images/right.png',180,180);
+    game.load.spritesheet('down','images/down.png',180,180);
 }
-var point;
-var pointSpeed=150;
+
+
+var checker;
+var checkerSpeed=80;
 var cursors;
 var spaceButton;
+var wave=[];
+var waveCheck=[];
+var arrow=[];
+//var up,down,left,right;
 var spaceTimer=0;
 var perfect;
 var good1,good2;
 var fair1,fair2;
+//var upIn,downIn,leftIn,rightIn;
 var wippo;
 var floor;
 var bg;
-var begin=false;
+var bgSpeed=0;
+var inGame=false;
 function create() {
     //game.world.setBounds(0, 0, 800, 1000);
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -35,64 +47,49 @@ function create() {
     //point.body.drag.set(70);
     //cursors = this.input.keyboard.createCursorKeys();
     spaceButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-
+    cursors = this.input.keyboard.createCursorKeys();
+    //////////////////////////////////////////////////////////////
     perfect = this.add.sprite(game.world.width/2,game.world.height*(4/5)+90,'lasers');
     perfect.scale.setTo(0.4,0.6);
-    //perfect.fixedToCamera = true;
     good1 = this.add.sprite(perfect.x+perfect.width,game.world.height*(4/5)+90,'lasers');
-    good1.scale.setTo(0.8,0.6);
+    good1.scale.setTo(0.4,0.6);
     good2 = this.add.sprite(perfect.x-good1.width,game.world.height*(4/5)+90,'lasers');
-    good2.scale.setTo(0.8,0.6);
+    good2.scale.setTo(0.4,0.6);
     fair1 = this.add.sprite(good1.x+good1.width,game.world.height*(4/5)+90,'lasers');
-    fair1.scale.setTo(1,0.6);
+    fair1.scale.setTo(0.4,0.6);
     fair2 = this.add.sprite(good2.x-fair1.width,game.world.height*(4/5)+90,'lasers');
-    fair2.scale.setTo(1,0.6);
+    fair2.scale.setTo(0.4,0.6);
+    ////////////////////////////////////////////////////////////
 
-    /*perfect.fixedToCamera = true;
-    perfect.cameraOffset.setTo(400, 500);
-    good1.fixedToCamera = true;
-    good1.cameraOffset.setTo(400+perfect.width,500);
-    good2.fixedToCamera = true;
-    good2.cameraOffset.setTo(400-good1.width,500);
-    fair1.fixedToCamera = true;
-    fair1.cameraOffset.setTo(400+perfect.width+good1.width,500);
-    fair2.fixedToCamera = true;
-    fair2.cameraOffset.setTo(400-perfect.width-fair1.width,500);*/
-
-    /*game.physics.arcade.enable(perfect);
-    game.physics.arcade.enable(good1);
-    game.physics.arcade.enable(good2);
-    game.physics.arcade.enable(fair1);
-    game.physics.arcade.enable(fair2);*/
-
-    point = this.add.sprite(0,game.world.height*(4/5)+120, 'laser');
-    point.anchor.set(0.5);
-    point.scale.setTo(0.05,0.6);
+    ////////////////////////////////////////////////////////////
+    checker = this.add.sprite(0,game.world.height*(4/5)+120, 'laser');
+    checker.anchor.set(0.5);
+    checker.scale.setTo(0.05,0.6);
     //point.fixedToCamera = true;
     // point.cameraOffset.setTo(200, 500);
-    game.physics.arcade.enable(point);
-    point.body.maxVelocity.set(1500);
-    point.body.collideWorldBounds = false;
-    wippo = this.add.sprite(game.world.width/2,game.world.height*(4/5)-80 ,'ship');
+    game.physics.arcade.enable(checker);
+    checker.body.maxVelocity.set(1500);
+    checker.body.collideWorldBounds = false;
+    wippo = this.add.sprite(game.world.width/2,game.world.height*(4/5)+15 ,'ship');
     game.physics.arcade.enable(wippo);
     floor = this.add.sprite(0,game.world.height*(4/5)+50,'laser');
     floor.scale.setTo(10,0.6);
     game.physics.arcade.enable(floor);
     floor.body.collideWorldBounds = false;
     floor.body.immovable = true;
-    game.time.events.add(Phaser.Timer.SECOND * 3, gameBegin, this);
+    game.time.events.add(Phaser.Timer.SECOND * 2, gameBegin, this);
     //floor.body.immovable = true;
     wippo.body.bounce.y = 0;
-    wippo.body.gravity.y = 200;
+    //wippo.body.gravity.y = 150;
     //wippo.events.onOutOfBounds.add(gameEnd(), this);
     //game.camera.follow(perfect);
 
-    this.score = 0;
+    /*this.score = 0;
     this.scoreText;
     this.scoreText = game.add.text(perfect.x, perfect.y-400, 'Score : ' + this.score, {
         fontSize: '20px',
         fill: '#ed3465'
-    })
+    })*/
 
 
 }
@@ -100,66 +97,80 @@ function create() {
 function update() {
     if(!this.game.world.bounds.intersects(wippo)){
         console.log(true);
-        gameEnd();
+        wippo.kill();
+        point.body.velocity.x = 0;
     }
-    if(begin)
+    if(inGame)
     {
-        this.scoreText.setText('Score : ' + this.score);
-        if(wippo.body.velocity.y<0)
-        {
-            bg.tilePosition.y -= (wippo.body.velocity.y-7)/2.5;
-            this.score+=bg.tilePosition.y/5000;
-        }else {
-            bg.tilePosition.y -= (wippo.body.velocity.y-7)/50;
-        }
+        //this.scoreText.setText('Score : ' + this.score);
+        bg.tilePosition.y += bgSpeed;
+        //for
         if (spaceButton.isDown&&game.time.now > spaceTimer)
         {
-            //wippo.body.velocity.y+=-300;
+             //wippo.body.velocity.y+=-300;
             check();
-
-            point.reset(0,game.world.height*(4/5)+120);
-            spaceTimer = game.time.now + 200;
+            //point.reset(0,game.world.height*(4/5)+120);
+            spaceTimer = game.time.now + 1000;
             //pointSpeed+=10;
         }
-        game.world.wrap(point, 16);
-        point.body.velocity.y=0;
-    	  point.body.velocity.x=pointSpeed;
+        game.world.wrap(checker, 16);
+        checker.body.velocity.y=0;
+    	  checker.body.velocity.x=checkerSpeed;
+
+        /*if(wippo.body.velocity.y<0){
+            wippo.body.velocity.y += 3;
+            bg.tilePosition.y += 30;
+            //console.log("kaw");
+        }else{
+          wippo.body.velocity.y = 0;
+        }*/
+        if(wippo.body.y-game.height/3<20){
+            wippo.body.velocity.y = 0;
+            bg.tilePosition.y += 18;
+            //console.log("kaw");
+        }else{
+            bg.tilePosition.y += 30;
+        }
     }
+    //console.log(wippo.body.velocity.y);
     game.physics.arcade.collide(wippo,floor);
+
 }
 function check(){
-    if (checkOverlap(point, perfect))
+    if (checkOverlap(checker, perfect))
     {
       console.log("Perfect!");
-      wippo.body.velocity.y=-150;
+      bgSpeed=40;
     }
-    else if (checkOverlap(point, good1))
+    else if (checkOverlap(checker, good1))
     {
       console.log("Good!");
-      wippo.body.velocity.y=-75;
+      bgSpeed=20;
     }
-    else if (checkOverlap(point, good2))
+    else if (checkOverlap(checker, good2))
     {
       console.log("Good!");
-      wippo.body.velocity.y=-75;
+      bgSpeed=20;
     }
-    else if (checkOverlap(point, fair1))
+    else if (checkOverlap(checker, fair1))
     {
       console.log("fair!");
-      wippo.body.velocity.y=-33;
+      bgSpeed=0;
     }
-    else if (checkOverlap(point, fair2))
+    else if (checkOverlap(checker, fair2))
     {
       console.log("fair!");
-      wippo.body.velocity.y=-25;
+      bgSpeed=0;
     }
     else
     {
       console.log("Bad");
+      bgSpeed=-30
     }
 
 
 }
+
 function checkOverlap(spriteA, spriteB) {
 
     var boundsA = spriteA.getBounds();
@@ -170,11 +181,69 @@ function checkOverlap(spriteA, spriteB) {
 }
 gameBegin = function (){
     floor.body.velocity.y = 400;
-    wippo.body.velocity.y= -300;
-    point.reset(0,game.world.height*(4/5)+120);
-    begin = true;
+    console.log("gameBegin");
+    //wippo.body.velocity.y = -360;
+    wippo.body.velocity.y = -100;
+    checker.reset(0,game.world.height*(4/5)+120);
+    inGame = true;
+    summonWave(3);
 }
-gameEnd = function (){
-    wippo.kill();
-    bg.tilePosition.y -= bg.tilePosition.y;
+function summonWave(numberWave){
+    var l = wave.length;
+    var startPositon;
+    if(numberWave==3){
+        x=game.world.width/2-50;
+        y=game.world.height*3/5;
+    }
+    for(var i=0;i<l;i++)
+    	 wave.pop();
+    destroyedCount=numberWave;
+    for (var i = 0; i < numberWave; i++){
+        wave.push(new arrowRandom(x,y));
+        x+=50;
+        console.log("looping");
+    }
 }
+arrowRandom = function (x,y) {
+    var rand = game.rnd.integerInRange(0, 3);
+    this.game = game;
+    //this.health = 3;
+    this.alive = true;
+    //this.enemy_ship = game.add.sprite(x, y, 'enemy_ship');
+
+    //this.enemy_ship.count=0;
+    //this.enemy_ship.countBullet=0;
+    if(rand==0){
+        this.up = game.add.sprite(x, y, 'up');
+        this.up.anchor.set(0.5);
+        this.up.scale.setTo(0.3, 0.3);
+        //this.up.name = index.toString();
+    }else if(rand==1){
+        this.left = game.add.sprite(x, y, 'left');
+        this.left.anchor.set(0.5);
+        this.left.scale.setTo(0.3, 0.3);
+        //this.left.name = index.toString();
+    }else if(rand==2){
+        this.right = game.add.sprite(x, y, 'right');
+        this.right.anchor.set(0.5);
+        this.right.scale.setTo(0.3, 0.3);
+        //this.right.name = index.toString();
+    }else{
+        this.down = game.add.sprite(x, y, 'down');
+        this.down.anchor.set(0.5);
+        this.down.scale.setTo(0.3, 0.3);
+        //this.down.name = index.toString();
+    }
+    //this.enemy_ship.anchor.set(0.5);
+    //this.enemy_ship.scale.setTo(0.3, 0.3);
+    //this.enemy_ship.name = index.toString();
+
+    /*up = this.add.sprite(x,y,'up');
+    up.scale.setTo(0.3,0.3);
+    left = this.add.sprite(x,y,'left');
+    left.scale.setTo(0.3,0.3);
+    right = this.add.sprite(x,y,'right');
+    right.scale.setTo(0.3,0.3);
+    down = this.add.sprite(x,y,'down');
+    down.scale.setTo(0.3,0.3);*/
+};
